c3a05caa559d94e8759f95449609bdd8d3cee1f4
k <ekaterina.d.loginova@gmail.com>
2023-09-30 17:40:57 +0200

    Moved prompts to text files. Added preliminary code for db creation and insertion.

--- scripts/get_chatgpt_criteria.py
+++ scripts/get_chatgpt_criteria.py
@@ -6,42 +6,20 @@ import openai
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-TYPE_PROMPT = "Определи, является ли текст: новостью, статьёй, записью в личном блоге, публичным комментарием или личным сообщением. Верни только категорию текста. Если текст не относится ни одной из этих категорий, верни 'другое'."
+PROMPTS_DIR = './prompts/'
+PROMPTS = {}
+for f in os.listdir(PROMPTS_DIR):
+    if f.endswith('_prompt.txt'):
+        with open(os.path.join(PROMPTS_DIR, f), 'r', encoding='utf-8') as f_in:
+            PROMPTS[f.replace('.txt', '')] = '\n'.join(f_in.readlines())
 
 SYSTEM_CONTENT = "Ты - журналист и исследователь российских СМИ, работающий в условиях диктатуры и военной цензуры (война между Россией и Украиной, с вмешательством Европы и США)."
-REFERENCES_PROMPT = """Есть ли в тексте ссылки на источники информации? Если они отсутствуют, верни только "Источники: не обнаружены."
-
-Текст:
-"""
-HATESPEECH_PROMPT = """Проверь текст на наличие дискриминирующих, стигматизирующих или провоцирующих ненависть лексики, высказываний или идей (например, трансфобию, гомофобию, национализм, расизм или сексизм).
-
-Текст:
-"""
-
-MANIPULATION_PROMPT = """Извлеки все приёмы манипуляции читателем из следующего текста и их краткое обоснование в виде пронумерованного списка. Если в тексте нет приёмов манипуляции, верни "Манипуляция: не обнаружена".
-
-Текст:
-"""
-
-LOGIC_ERR_PROMPT = """Проверь текст (внутри тройной `) на наличие логических ошибок или когнитивных искажений. Если они не обнаружены в тексте, напиши, что "Логические ошибки: не обнаружены". Если они, то сформулируй свой ответ так:
-1) Наличие логических ошибок: поочередно перечисли название логических ошибок и для каждой из них кратко (одно предложение) объясни на примере текста, почему это логическая ошибка.
-2) Поправка: кратко (в двух предложениях) объясни, почему обнаруженные логические ошибки опасны, и как их избежать.
-
-Например:
-
-Наличие логических ошибок:
-1) Атака личности. Уоррен атакует личность Уилла, обвиняя его в ненависти к стране, вместо того чтобы обсуждать его аргументы по поводу военных расходов.
-2) Подмена тезиса: Уоррен искажает слова Уилла и потом опровергает своё искажение.
-
-Поправка: Обнаруженные логические ошибки опасны, так как отвлекают от рассмотрения фактов и аргументов, и могут вести к искаженному восприятию ситуации. Чтобы избежать их, важно сосредотачиваться на деле и обсуждении аргументов, а не переходить на личности собеседников.
-"""
-
 
 def process_text(text):
     run_analysis = False
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
-        messages=[{"role": "user", "content": f"{TYPE_PROMPT} Текст: '{text}'"}],
+        messages=[{"role": "user", "content": f"{PROMPTS['type_prompt']} Текст: '{text}'"}],
         temperature=0,
         top_p=1,
         frequency_penalty=0,
@@ -58,9 +36,10 @@ def process_text(text):
 
 def analyse_text(text):
     field2prompt = {
-        "manipulation_methods": MANIPULATION_PROMPT,
-        "hatespeech": HATESPEECH_PROMPT,
-        "references": REFERENCES_PROMPT,
+        "manipulation_methods": PROMPTS['manipulation_prompt'],
+        "hatespeech": PROMPTS['hatespeech_prompt'],
+        "references": PROMPTS['references_prompt'],
+        "logical_fallacies": PROMPTS['logical_fallacy_prompt'],
     }
     criteria = {field: None for field in field2prompt.keys()}
     for field_name, prompt in field2prompt.items():
@@ -85,6 +64,10 @@ def analyse_text(text):
         criteria["manipulation_present"] = 0
     else:
         criteria["manipulation_present"] = 1
+    if "Логические ошибки: не обнаружены" in criteria["logical_fallacies"]:
+        criteria["manipulation_present"] = 0
+    else:
+        criteria["manipulation_present"] = 1
     return criteria
 
 
@@ -93,7 +76,9 @@ def main(text):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Run ChatGPT prompts for the given text.")
+    parser = argparse.ArgumentParser(
+        description="Run ChatGPT prompts for the given text."
+    )
     parser.add_argument("text", type=str, help="Input text to be processed.")
 
     args = parser.parse_args()

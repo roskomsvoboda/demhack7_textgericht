2a8a476e7979c5d31e20913c386dabf19633a073
Konovalov Evgeny <pilon4ik@mail.ru>
2023-10-01 16:14:10 +0200

    Update main.py

--- source/main.py
+++ source/main.py
@@ -133,7 +133,7 @@ def main() -> None:
     application.add_error_handler(error)
     application.add_handler(CallbackQueryHandler(feedback_button))
 
-    application.add_handler(MessageHandler(filters.ChatType.PRIVATE & filters.TEXT & ~filters.COMMAND, text_check))
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, text_check))
 
     application.run_polling(allowed_updates=Update.ALL_TYPES)
 
